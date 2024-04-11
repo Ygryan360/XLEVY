28b1a995804d11f00a68578c9d66556ad9f8a996
Rayane Tchabodi <139383014+Ygryan360@users.noreply.github.com>
2024-04-11 23:28:07 +0000

    Ajout des barres de progression et du  bg welcome

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Script pour la barre de navigation
+
 let button = document.querySelector(".button");
 let menu = document.querySelector(".menu");
 
@@ -5,3 +7,27 @@ button.onclick = function () {
   button.classList.toggle("active");
   menu.classList.toggle("active");
 };
+
+// script pour les stats
+window.onload = function () {
+  animateSkills();
+};
+
+function animateSkills() {
+  var skills = document.querySelectorAll(".skills");
+  skills.forEach(function (skill) {
+    var endValue = parseInt(skill.textContent);
+    var duration = 3000;
+    var increment = endValue / (duration / 10);
+    var current = 0;
+
+    var timer = setInterval(function () {
+      if (current >= endValue) {
+        clearInterval(timer);
+      } else {
+        current += increment;
+        skill.textContent = Math.round(current) + "%";
+      }
+    }, 10);
+  });
+}
